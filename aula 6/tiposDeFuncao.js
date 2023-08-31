7a9c7c164edbada3242fa458bc60154aa130b0db
Joice Cristina <joice1509oliveira@gmail.com>
2023-08-31 10:57:42 -0300

    finalizando as atividades prática deste curso

---
+++ aula 6/tiposDeFuncao.js
@@ -0,0 +1,18 @@
+//declarativa
+function funcaoDeclarativa() {
+    console.log('Sou uma mensagem de uma função declarativa!');
+}
+
+funcaoDeclarativa();
+
+//expressao
+var funcaoExpressao = function() {
+    console.log('Sou uma mensagem de uma função de expressão!')
+}
+
+funcaoExpressao();
+
+//arrow function
+var funcaoArrowFunction = () => {
+    console.log('Sou uma arrow function!');
+}
